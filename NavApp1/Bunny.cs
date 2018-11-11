d1c624d607f1c060133ba24ad5df8492106b0029
Aaron Montague <montaa@alum.rpi.edu>
2018-11-11 00:16:58 -0500

    Added Skunk Picture

---
+++ NavApp1/Bunny.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace NavApp1
+{
+    [Activity(Label = "@string/viewBunny", Theme = "@style/AppTheme", MainLauncher = false)]
+    public class Bunny : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Make it show up...
+            SetContentView(Resource.Layout.bunny);
+        }
+    }
+}
--- NavApp1/MainActivity.cs
+++ NavApp1/MainActivity.cs
@@ -3,6 +3,7 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using Android.Content;
 
 namespace NavApp1
 {
@@ -14,6 +15,26 @@ namespace NavApp1
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            //Go to Bunny View
+            Button bunnyButton = FindViewById<Button>(Resource.Id.buttonBunny);
+            bunnyButton.Click += BunnyButton_Click;
+
+            //Go to Skunk View
+            Button skunkButton = FindViewById<Button>(Resource.Id.buttonSkunk);
+            skunkButton.Click += SkunkButton_Click;
+        }
+
+        private void BunnyButton_Click(object sender, System.EventArgs e)
+        {
+            Intent intentBunny = new Intent(this, typeof(Bunny));
+            StartActivity(intentBunny);
+        }
+
+        private void SkunkButton_Click(object sender, System.EventArgs e)
+        {
+            Intent intentSkunk = new Intent(this, typeof(Skunk));
+            StartActivity(intentSkunk);
         }
     }
 }
